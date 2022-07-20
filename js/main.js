82e8736e07d82eaae5c28b9febb25988567972e0
brandon <brandonrjguthrie@gmail.com>
2022-07-19 19:52:47 -0700

    Create index.html with boilerplate with link to javascript and comment how program will work in main.js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//Write a function called 'playRound()' that will play a single round of 'Rock Paper Scissors'.
+
+
+    //Prompt for player selection 
+    //Make the selection case insensitive
+    //If the player has not entered a valid option, restart round and prompt again
+    //Randomly assign the computers selection.
+        //by generating a number between 1 and 3
+        //and assigning 'rock', 'paper', or 'scissors' as values instead of 1,2 or 3
+    //Determine if the player won and console log a string that declares the winner of the round in the format "You Lose! Paper beats Rock."
+    //return the result
+
+
+//Create a new function called 'game()' which calls the 'playRound' function 5 times,
+//track the score
+//determine and declares a winner, loser, or tie game.
