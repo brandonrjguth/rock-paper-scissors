36b74794b9ec92cd983218ecc4a4a026a3f59988
brandon <brandonrjguthrie@gmail.com>
2022-07-19 20:04:28 -0700

    in playRound, randomly generate the computers selection

--- js/main.js
+++ js/main.js
@@ -16,7 +16,26 @@ let playRound = () => {
 
     //Randomly assign the computers selection
     //by generating a number between 1 and 3
+    let computerSelection = Math.Floor(Math.random()*3 + 1);
+
     //and assign 'Rock', 'Paper', or 'Scissors' equivalent to possible numerical results
+    switch (computerSelection){
+
+        case 1:
+            computerSelection = "Rock";
+            break;
+        
+        case 2: 
+            computerSelection = "Paper";
+            break;
+
+        case 3:
+            computerSelection = "Scissors";
+            break;
+
+    }
+
+    
     //Determine if the player won and console log a string that declares the winner of the round in the format "You Lose! Paper beats Rock."
     //return the result
 
