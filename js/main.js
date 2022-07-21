8c0295015bc5b6c04dadca93d38e34fe50b94d64
brandon <brandonrjguthrie@gmail.com>
2022-07-20 17:25:37 -0700

    write game() function to play 5 rounds and declare result out of 5

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ let playRound = () => {
     //Prompt for player selection 
     let playerSelectionPrompt = () => {
     
-        playerSelection = prompt();
+        playerSelection = prompt("Please choose 'Rock', 'Paper', or 'Scissors'");
 
          //Make the selection case insensitive
         playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
@@ -51,27 +51,76 @@ let playRound = () => {
         playerSelection === "Paper" && computerSelection === "Rock"){
 
         result = "Win";
+
         console.log(`You ${result}! ${playerSelection} beats ${computerSelection}.`)
+        alert(`You ${result}! ${playerSelection} beats ${computerSelection}.`)
+        
         
 
     } else if (playerSelection === computerSelection){
 
         result = "Tied";
+
         console.log(`You ${result}!`)
+        alert(`You ${result}!`)
+        
 
     } else {
 
         result = "Lost";
-        console.log(`You ${result}! ${computerSelection} beats ${playerSelection}.`)
 
+        console.log(`You ${result}! ${computerSelection} beats ${playerSelection}.`)
+        alert(`You ${result}! ${computerSelection} beats ${playerSelection}.`)
+        
     }
 
     //return the result
     return result;
+}
+
 
 
 
 //Create a new function called 'game()' which calls the 'playRound' function 5 times,
-//track the score
-//determine and declares a winner, loser, or tie game.
+let game = () => {
+
+    let playerTotalScore = 0;
+    let computerTotalScore = 0;
+
+    for (i=0; i<5; i++){
+
+        let result = playRound();
+
+        //track the score
+        if (result === "Win"){
+            playerTotalScore++;
+        }
+
+        else if (result === "Lost"){
+            computerTotalScore++;
+        }
+    }
+
+    //Determine winner out of 5
+    let finalResult;
+
+    if (playerTotalScore > computerTotalScore){
+
+        finalResult = "Won";
+
+    } else if (playerTotalScore < computerTotalScore){
+
+       finalResult = "Lost";
+        
+    } else {
+
+        finalResult = "Tied";
+
+    }
+
+    console.log(`Your score is ${playerTotalScore}. The computers score is ${computerTotalScore}. You ${finalResult}!`)
+    alert(`Your score is ${playerTotalScore}. The computers score is ${computerTotalScore}. You ${finalResult}!`)
+    
+
 }
+
